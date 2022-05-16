ad3d188a832d2b1610c0bc6f63e8205cb8cc3cca
Eleonora Kazakova <abei-2005@yandex.ru>
2022-05-16 13:59:56 +0200

    Refactoring

--- src/components/admin/EpisodeEdit.jsx
+++ src/components/admin/EpisodeEdit.jsx
@@ -1,101 +1,77 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { createFile } from "../../scripts/cloudStorage";
-import { getDocument, updateDocument } from "../../scripts/fireStore";
-import EmptyImg from "../../images/empty.jpg";
-import uploadFiles from "../../scripts/uploadFile";
-import { useModal } from "../../state/ModalProvider";
 import "../../styles/form.sass";
-import InputField from "../InputField";
+import { createFile } from "../../scripts/cloudStorage";
+import { updateDocument } from "../../scripts/fireStore";
+import { useModal } from "../../state/ModalProvider";
+import { useState } from "react";
 import createFormSeries from "../../data/createFormSeries.json";
+import EmptyImg from "../../images/empty.jpg";
 import FormPicture from "./FormPicture";
+import InputFieldEvent from "../InputFieldEvent";
 
 export default function EpisodeEdit({ stateSeries, episode }) {
   const [series, setSeries] = stateSeries;
   const { setModal } = useModal();
-  const [status, setStatus] = useState(1);
   const [file, setFile] = useState(null);
   const [currentEpisode, setCurrentEpisode] = useState(episode);
-
-  console.log("series:", series);
 
   if (series === null) return null;
 
   async function onUpdate(event) {
-    try {
-      event.preventDefault();
+    event.preventDefault();
 
-      if (currentEpisode.imgURL === "") {
-        currentEpisode.imgURL = EmptyImg;
-      } else if (file !== null) {
-        const imgURL = await createFile(
-          `series-${series.title}/${file.name}`,
-          file
-        );
-        currentEpisode.imgURL = imgURL;
-      }
-
-      setStatus(0);
-
-      setSeries({
-        ...series,
-        seasons: series.seasons.map((el) =>
-          el.id === currentEpisode.id ? currentEpisode : el
-        ),
-      });
-
-      await updateDocument(`netflixClone/series/content/${series.id}`, {
-        ...series,
-      });
-
-      setModal(null);
-    } catch (error) {
-      console.error("There was an error:", error);
-
-      setStatus(2);
+    if (currentEpisode.imgURL === "") {
+      currentEpisode.imgURL = EmptyImg;
+    } else if (file !== null) {
+      const imgURL = await createFile(
+        `series-${series.title}/${file.name}`,
+        file
+      );
+      currentEpisode.imgURL = imgURL;
     }
+
+    setSeries({
+      ...series,
+      seasons: series.seasons.map((el) =>
+        el.id === currentEpisode.id ? currentEpisode : el
+      ),
+    });
+
+    await updateDocument(`netflixClone/series/content/${series.id}`, {
+      ...series,
+    });
+
+    setModal(null);
+  }
+
+  function onChangeTitle(event) {
+    setCurrentEpisode({ ...currentEpisode, title: event.target.value });
+  }
+  function onChangeDescription(event) {
+    setCurrentEpisode({ ...currentEpisode, description: event.target.value });
+  }
+
+  function onChangeLink(event) {
+    setCurrentEpisode({ ...currentEpisode, link: event.target.value });
   }
 
   return (
     <div>
       <h2>Edit {episode.title}</h2>
-      <div>
-        <label>Title of the episode</label>
-        <input
-          placeholder="Title"
-          required
-          type="text"
-          value={currentEpisode.title}
-          onChange={(event) =>
-            setCurrentEpisode({ ...currentEpisode, title: event.target.value })
-          }
-        />
-      </div>
-      <div>
-        <label>Description of the episode</label>
-        <input
-          placeholder="description"
-          type="text"
-          value={currentEpisode.description}
-          onChange={(event) =>
-            setCurrentEpisode({
-              ...currentEpisode,
-              description: event.target.value,
-            })
-          }
-        />
-      </div>
-      <div>
-        <label>Link of the episode</label>
-        <input
-          placeholder="link"
-          type="text"
-          value={series.link}
-          onChange={(event) =>
-            setCurrentEpisode({ ...currentEpisode, link: event.target.value })
-          }
-        />
-      </div>
+      <InputFieldEvent
+        setup={createFormSeries.episodeTitle}
+        onChange={onChangeTitle}
+        value={currentEpisode.title}
+      />
+      <InputFieldEvent
+        setup={createFormSeries.episodeDescription}
+        onChange={onChangeDescription}
+        value={currentEpisode.description}
+      />
+      <InputFieldEvent
+        setup={createFormSeries.episodeLink}
+        onChange={onChangeLink}
+        value={currentEpisode.link}
+      />
       <FormPicture state={[file, setFile]} />
       <button onClick={onUpdate}>Edit episode</button>
     </div>
