19dd63ad39c3b23531ec7de56bb7a9d0d3537f84
Eleonora Kazakova <abei-2005@yandex.ru>
2022-05-19 15:36:56 +0200

    Add thumbsUp-state

--- src/components/user/UserCategory.jsx
+++ src/components/user/UserCategory.jsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 import { getCollection, deleteDocument } from "../../scripts/fireStore";
 import "../../styles/basic/card.sass";
 import Youtube from "../Youtube";
+import Play from "../../images/modal/play.svg";
+import Plus from "../../images/modal/plus.svg";
+import ThumbsUp from "../../images/modal/thumbs-up.svg";
+import ThumbsDown from "../../images/modal/thumbs-down.svg";
+import "../../styles/user-category.sass";
 
 import { useModal } from "../../state/ModalProvider";
 import { Link } from "react-router-dom";
@@ -13,6 +18,7 @@ export default function UserCategory({ category }) {
   const { setModal } = useModal();
   const [videos, setVideos] = useState([]);
   const path = `netflixClone/${category.id}/content`;
+  const [openThumb, setOpenThumb] = useState(false);
 
   useEffect(() => {
     async function loadData(path) {
@@ -22,21 +28,41 @@ export default function UserCategory({ category }) {
     loadData(path);
   }, []);
 
+  function openThumbs() {
+    setOpenThumb(!openThumb);
+  }
+
+  function openModal(event, video) {
+    event.preventDefault();
+    setModal(
+      <div>
+        <Youtube link={video.link} />
+        <img src={Play} className="user-category-button" />
+        <img src={Plus} className="user-category-icon" />
+        <img
+          src={ThumbsUp}
+          className="user-category-icon"
+          onClick={openThumbs}
+        />
+        {!openThumb ? (
+          <>
+            <img src={ThumbsUp} className="user-category-icon" />
+            <img src={ThumbsDown} className="user-category-icon" />
+          </>
+        ) : null}
+        {video.title}
+        {video.description}
+      </div>
+    );
+  }
+
   const videoCards = videos.map((video) => (
     <div>
       {video.title}
       <img
         src={video.imgURL}
         className="card-img"
-        onClick={() =>
-          setModal(
-            <div>
-              <Youtube link={video.link} />
-              {video.title}
-              {video.description}
-            </div>
-          )
-        }
+        onClick={(event) => openModal(event, video)}
       />
     </div>
   ));
