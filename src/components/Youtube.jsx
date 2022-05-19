3a4258dfeaa1d5e3d4dedc389b0af34cb1bdc23f
Eleonora Kazakova <abei-2005@yandex.ru>
2022-05-19 12:51:13 +0200

    Add styles

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -4,8 +4,6 @@ export default function Youtube({ link }) {
   return (
     <div className="youtube-responsive">
       <iframe
-        width="453"
-        height="280"
         src={`https://www.youtube.com/embed/${link}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
