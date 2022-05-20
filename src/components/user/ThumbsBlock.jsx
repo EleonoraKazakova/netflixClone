4fdce001070fe212991164d9c123cb56a028821b
Eleonora Kazakova <abei-2005@yandex.ru>
2022-05-20 10:00:43 +0200

    Impruve ThumbsBlock

--- src/components/user/ThumbsBlock.jsx
+++ src/components/user/ThumbsBlock.jsx
@@ -10,27 +10,36 @@ export default function ThumbsBlock() {
   const [openThumb, setOpenThumb] = useState(false);
   const [openThumbUp, setOpenThumbUp] = useState(false);
   const [openThumbDown, setOpenThumbDown] = useState(false);
+  const [thumb, setThumb] = useState(0);
 
   function changeThumbsUp() {
     setOpenThumbUp(!openThumbUp);
     setOpenThumbDown(false);
+    setThumb(1);
   }
 
   function changeThumbsDown() {
     setOpenThumbDown(!openThumbDown);
     setOpenThumbUp(false);
+    setThumb(2);
+  }
+
+  let image;
+  switch (thumb) {
+    case 1:
+      image = ThumbsUpActive;
+      break;
+    case 2:
+      image = ThumbsDownActive;
+      break;
+    default:
+      image = ThumbsUp;
   }
 
   return (
     <div className="thumbsblock-block">
-      {/* <img
-        src={ThumbsUp}
-        className="user-category-icon"
-        onMouseOver={() => setOpenThumb(true)}
-        onMouseOut={() => setOpenThumb(false)}
-  />*/}
       {openThumb ? (
-        <div>
+        <div onClick={() => setOpenThumb(false)}>
           <img
             src={openThumbUp ? ThumbsUpActive : ThumbsUp}
             className="user-category-icon"
@@ -44,7 +53,7 @@ export default function ThumbsBlock() {
         </div>
       ) : (
         <img
-          src={ThumbsUp}
+          src={image}
           className="user-category-icon"
           onClick={() => setOpenThumb(!openThumb)}
         />
--- src/scripts/firesbase.js
+++ src/scripts/firesbase.js
@@ -3,23 +3,23 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-/*const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyB2b6k89vCH_D6ImUenEpkCDVDaSq6lJlo",
   authDomain: "netflix-5761b.firebaseapp.com",
   projectId: "netflix-5761b",
   storageBucket: "netflix-5761b.appspot.com",
   messagingSenderId: "508637399299",
   appId: "1:508637399299:web:8200ef1f24f5a031ef8136",
-};*/
+};
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyDjof7ZquoN1Q5X3XNcMPSmsW2Hs8ay6LM",
   authDomain: "netflix-2063c.firebaseapp.com",
   projectId: "netflix-2063c",
   storageBucket: "netflix-2063c.appspot.com",
   messagingSenderId: "733618508223",
   appId: "1:733618508223:web:799817d6ee51dde2c2d10a",
-};
+};*/
 
 const firebaseapp = initializeApp(firebaseConfig);
 export const fireStore = getFirestore(firebaseapp);
