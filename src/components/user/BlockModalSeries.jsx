89d917e68c7dbf8b2c9b8b07fd33f1b5fabd7228
Eleonora Kazakova <abei-2005@yandex.ru>
2022-05-21 14:54:21 +0200

    Add styles

--- src/components/user/BlockModalSeries.jsx
+++ src/components/user/BlockModalSeries.jsx
@@ -4,7 +4,8 @@ import CaretDown from "../../images/modal/caret-down.svg";
 
 export default function BlockModalSeries({ video, category }) {
   const [currentSeason, setCurrentSeason] = useState("1");
-
+  const [openSeasons, setOpenSeasons] = useState(false);
+  console.log("openSeasons:", openSeasons);
   const uniqueSeasons = [...new Set(video.seasons.map((vid) => vid.season))];
 
   const season = uniqueSeasons.map((season) => (
@@ -32,24 +33,35 @@ export default function BlockModalSeries({ video, category }) {
         <p className="user-category-match">{video.match}% Match</p>
         {video.year}
       </div>
+
       <p className="user-category-match-year"> {video.description}</p>
 
       <div className="user-category-seasons">
         <h2>Episodes</h2>
 
         <div className="series-page-dropdown">
-          <p>Choose season</p>
-          <img src={CaretDown} className="series-page-caretdown" />
-          <div className="series-page-dropdown-content">{season}</div>
+          <div
+            onClick={() => setOpenSeasons(!openSeasons)}
+            className="series-page-seasons"
+          >
+            <p>Choose season</p>
+            <img src={CaretDown} className="series-page-caretdown" />
+          </div>
+
+          {openSeasons && (
+            <div className="series-page-dropdown-content">{season}</div>
+          )}
         </div>
       </div>
 
       {episodes.map((season) => (
         <div className="user-category-series">
           <img src={season.imgURL} className="user-category-img" />
-          {season.episode}
-          {season.title}
-          {season.description}
+          <div className="user-category-series-content">
+            <p>{season.episode}</p>
+            <p>{season.title}</p>
+            <p>{season.description}</p>
+          </div>
         </div>
       ))}
     </div>
